0a65f1b069ef19b6b79aa68de75c3b7678c80879
mrocco64 <Mike@mikes-air.att.net>
2014-08-20 11:48:41 -0700

    [UPDATE] trigger campaign api call and assisting methods

--- lib/responsys/api/all.rb
+++ lib/responsys/api/all.rb
@@ -2,6 +2,7 @@ require "responsys/api/folder"
 require "responsys/api/list"
 require "responsys/api/session"
 require "responsys/api/table"
+require "responsys/api/campaign"
 
 module Responsys
   module Api
@@ -10,6 +11,7 @@ module Responsys
       include Responsys::Api::List
       include Responsys::Api::Session
       include Responsys::Api::Table
+      include Responsys::Api::Campaign
     end
   end
 end
---
+++ lib/responsys/api/campaign.rb
@@ -0,0 +1,31 @@
+require "responsys/api/object/all"
+
+module Responsys
+  module Api
+    module Campaign
+
+      def trigger_message(campaign, recipients)
+        recipients = hash_recipients(recipients)
+        message = {
+          campaign: campaign.to_hash,
+          recipientData: recipients 
+        }
+        outcome = api_method(:trigger_campaign_message, message)
+        check_errors(outcome, recipients)
+      end
+      def hash_recipients(recipients)
+        if recipients.respond_to?(:each)
+          hashedRecipients = []
+          recipients.each {|recipient| hashedRecipients<<recipient.to_hash}
+          recipients = hashedRecipients
+        else
+          recipients = recipients.to_hash
+        end
+        recipients
+      end
+      def check_errors(outcome, recipients)
+        outcome.each_index {|i| puts recipients[i] unless outcome[i][:success]}
+      end
+    end
+  end
+end

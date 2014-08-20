53fb995b5bbb778733b37757947756ebd4e37270
mrocco64 <Mike@mikes-air.att.net>
2014-08-20 11:47:43 -0700

    [UPDATE] first steps of recipient and recipient_data objects

---
+++ lib/responsys/api/object/recipient.rb
@@ -0,0 +1,32 @@
+module Responsys
+  module Api
+    module Object
+      class Recipient
+        include Responsys::Api::Object
+        attr_accessor :listName, :recipientId, :customerId, :emailAddress, :mobileNumber, :emailFormat
+
+
+        def initialize(options = {})
+          @listName = options[:listName] || InteractObject.new("", "")
+          @recipientId = options[:recipientId] || ""
+          @customerId = options[:customerId] || ""
+          @emailAddress = options[:emailAddress] || ""
+          @mobileNumber = options[:mobileNumber] || ""
+          @emailFormat = options[:emailFormat] || ""
+        end
+
+        def to_hash
+          { 
+            listName: @listName.to_hash,
+            recipientId: @recipientId.to_i,
+            customerId: @customerId.to_s,
+            emailAddress: @emailAddress.to_s,
+            mobileNumber: @mobileNumber.to_s,
+            emailFormat: @emailFormat 
+          }
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/responsys/api/object/recipient_data.rb
@@ -0,0 +1,29 @@
+module Responsys
+  module Api
+    module Object
+      class RecipientData
+        attr_accessor :recipient, :optionalData
+
+        def initialize(recipient, optionalData = nil)
+          @recipient = recipient
+          @optionalData = optionalData.nil? ? default_optional_data : optionalData
+        end
+
+        def to_hash
+          {
+            recipient: @recipient.to_hash,
+            optionalData: @optionalData
+          }
+        end
+
+        def default_optional_data
+          [{
+            name: "",
+            value: ""
+          }]
+        end
+
+      end
+    end
+  end
+end

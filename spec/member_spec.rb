81f1e0e8a22a7242d9e896d6cc3ecc1eddf02f5f
Florian Lorrain <florian.lorrain@thredup.com>
2014-08-20 14:36:22 -0700

    [FEAT] Member retrieving profile extension data

--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -4,16 +4,19 @@ require "responsys/api/client"
 describe Responsys::Member do
 
   context "New member" do
+
     xit "should call mergeListMembers" do
     end
+
   end
 
   context "Subscribing" do
+
     before(:each) do
       @connection = double "connection"
 
       allow(Responsys::Api::Client).to receive(:instance).and_return(@connection)
-      
+
       @member = Responsys::Member.new("user@email.com")
       @list = Responsys::Api::Object::InteractObject.new("list_folder","list_object_name")
     end
@@ -23,14 +26,16 @@ describe Responsys::Member do
 
     it "should check the user has subscribed" do
       response_expected = { :record_data => { :field_names => "EMAIL_PERMISSION_STATUS_", :records => { :field_values=>"I" } } }
-      
-      allow(@connection).to receive(:retrieve_list_members).with(@list, "EMAIL_ADDRESS", %w(EMAIL_PERMISSION_STATUS_), %W(@member.email)).and_return(response_expected)
+
+      allow(@connection).to receive(:retrieve_list_members).with(@list, "EMAIL_ADDRESS", %w(EMAIL_PERMISSION_STATUS_), %W(#{@member.email})).and_return(response_expected)
 
       expect(@member.subscribed?(@list)).to eq(true)
     end
+
   end
 
   context "Existing member" do
+
     before(:each) do
       @connection = double "connection"
 
@@ -42,5 +47,43 @@ describe Responsys::Member do
 
     xit "should check its existance to the list" do
     end
+
+  end
+
+  context "Get profile extension data" do
+
+    before(:each) do
+      VCR.use_cassette("member/login") do
+        @profile_extension = Responsys::Api::Object::InteractObject.new("demo_folder2", "customers_extension")
+        @member_without_riid = Responsys::Member.new("user@email.com")
+        @member_with_riid = Responsys::Member.new("user@email.com", 398403)
+        @fields = %w(RIID_ EMAIL_ADDRESS)
+      end
+    end
+
+    it "should set the status to failure if no riid provided" do
+      VCR.use_cassette("member/retrieve_profile_extension_fail") do
+        response = @member_without_riid.retrieve_profile_extension(@profile_extension, @fields)
+
+        expect(response[:status]).to eq("failure")
+      end
+    end
+
+    it "should set a i18n message in the response if no riid provided" do
+      VCR.use_cassette("member/retrieve_profile_extension_fail") do
+        response = @member_without_riid.retrieve_profile_extension(@profile_extension, @fields)
+
+        expect(response[:error][:message]).to eq(I18n.t("member.riid_missing"))
+      end
+    end
+
+    it "should set the status to ok" do
+      VCR.use_cassette("member/retrieve_profile_extension") do
+        response = @member_with_riid.retrieve_profile_extension(@profile_extension, @fields)
+
+        expect(response[:status]).to eq("ok")
+      end
+    end
+
   end
 end

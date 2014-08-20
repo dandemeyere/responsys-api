de47577bb464511a404d53131cfb7aeedcb1bfb3
Florian Lorrain <florian.lorrain@thredup.com>
2014-08-20 14:36:22 -0700

    [FEAT] Member retrieving profile extension data

--- lib/responsys/member.rb
+++ lib/responsys/member.rb
@@ -7,43 +7,36 @@ module Responsys
     include Responsys::Api::Object
     attr_accessor :email
 
-    # Creates a Member object.
-    # Params:
-    # +email+:: email of the member
-    def initialize(email)
+    def initialize(email, riid = nil)
       @email = email
+      @user_riid = riid
       @client = Client.instance
     end
 
-    # Update the information of the Member.
-    # Params:
-    # +list+:: +Responsys::Api::Object::Interactobject+ which representents the list to update.
-    # +data+:: +Hash+ composed of all the data to update as {field1:value1, field2:value2}
     def update(list, data)
       record = RecordData.new(data.keys, data.values)
       @client.merge_list_members(list, record)
     end
 
-    # Subscribe the Member to the list. This is an update of the field "EMAIL_PERMISSION_STATUS_" to OptIn char "I".
-    # Params:
-    # +list+:: +Responsys::Api::Object::Interactobject+ which representents the list.
     def subscribe(list)
       update(list, { :EMAIL_ADDRESS_ => @email, :EMAIL_PERMISSION_STATUS_ => "I" })
     end
 
-    # Give the subscribe status of the Member for the list
-    # Params:
-    # +list+:: +Responsys::Api::Object::Interactobject+ which representents the list.
     def subscribed?(list)
       response = @client.retrieve_list_members(list, "EMAIL_ADDRESS", %w(EMAIL_PERMISSION_STATUS_), %W(#{@email}))
       response[:record_data][:records][:field_values] == "I"
     end
 
-    # Unsubscribe the Member to the list. This is an update of the field "EMAIL_PERMISSION_STATUS_" to OptOut char "O".
-    # Params:
-    # +list+:: +Responsys::Api::Object::Interactobject+ which representents the list.
     def unsubscribe(list)
       update(list, { :EMAIL_ADDRESS_ => @email, :EMAIL_PERMISSION_STATUS_ => "O" })
+    end
+
+    def retrieve_profile_extension(profile_extension, fields)
+      if @user_riid.nil?
+        Responsys::Helper.format_response_with_message("member.riid_missing")
+      else
+        @client.retrieve_profile_extension_records(profile_extension, QueryColumn.new("RIID"), fields, [@user_riid])
+      end
     end
   end
 end

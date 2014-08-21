7ad7bd9b567e2edec1109f86e877d94cada900c8
mrocco64 <Mike@Mikes-Air.local>
2014-08-21 15:38:16 -0700

    [UPDATE] email_format api object

---
+++ lib/responsys/api/object/email_format.rb
@@ -0,0 +1,23 @@
+module Responsys
+  module Api
+    module Object
+      class EmailFormat
+        include Responsys::Api::Object
+        attr_accessor :email_format_string
+        AVAILABLE_EMAIL_FORMAT = %w(TEXT_FORMAT HTML_FORMAT NO_FORMAT MULTIPART_FORMAT)
+
+        def initialize(email_format = "NO_FORMAT")
+          if AVAILABLE_EMAIL_FORMAT.include? email_format
+            self.email_format_string = email_format
+          #else
+          #  raise ParameterException, I18n.t("api.object.email_format.incorrect")
+          end
+        end
+
+        def to_api
+          email_format_string
+        end
+      end
+    end
+  end
+end

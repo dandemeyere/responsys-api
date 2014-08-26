9c7724101e4f7be00f8ea78e32e547b4d71adac7
Florian Lorrain <florian.lorrain@thredup.com>
2014-08-26 14:59:44 -0700

    [UPDATE] The Member class checks if the user is in the list before making any actions and return a failure hash if so

--- lib/responsys/member.rb
+++ lib/responsys/member.rb
@@ -14,32 +14,55 @@ module Responsys
     end
 
     def add_to_list(list, subscribe = false)
+      return Responsys::Helper.format_response_with_message("member.record_not_found") unless present?(list)
+
       data = { EMAIL_ADDRESS_:  @email, EMAIL_PERMISSION_STATUS_: subscribe ? "I" : "O" }
       record = RecordData.new([data])
+
       @client.merge_list_members_riid(list, record, ListMergeRule.new(insertOnNoMatch: true, updateOnMatch: "NO_UPDATE"))
     end
 
-    def update(list, data)
-      record = RecordData.new(data)
-      @client.merge_list_members(list, record)
+    def retrieve_profile_extension(profile_extension, fields)
+      return Responsys::Helper.format_response_with_message("member.riid_missing") if @user_riid.nil?
+      return Responsys::Helper.format_response_with_message("member.record_not_found") unless present?(profile_extension)
+
+      @client.retrieve_profile_extension_records(profile_extension, QueryColumn.new("RIID"), fields, [@user_riid])
     end
 
     def subscribe(list)
       update(list, [{ EMAIL_ADDRESS_: @email, EMAIL_PERMISSION_STATUS_: "I" }])
     end
 
-    def subscribed?(list)
-      response = @client.retrieve_list_members(list, QueryColumn.new("EMAIL_ADDRESS"), %w(EMAIL_PERMISSION_STATUS_), [@email])
-      response[:data][0][:EMAIL_PERMISSION_STATUS_] == "I"
-    end
-
     def unsubscribe(list)
       update(list, [{ EMAIL_ADDRESS_: @email, EMAIL_PERMISSION_STATUS_: "O" }])
     end
 
-    def retrieve_profile_extension(profile_extension, fields)
-      return Responsys::Helper.format_response_with_message("member.riid_missing") if @user_riid.nil?
-      @client.retrieve_profile_extension_records(profile_extension, QueryColumn.new("RIID"), fields, [@user_riid])
+    def update(list, data)
+      return Responsys::Helper.format_response_with_message("member.record_not_found") unless present?(list)
+
+      record = RecordData.new(data)
+      @client.merge_list_members(list, record)
+    end
+
+    def present?(list)
+      response = @client.retrieve_list_members(list, QueryColumn.new("EMAIL_ADDRESS"), %w(EMAIL_PERMISSION_STATUS_), [@email])
+
+      return false if response[:status] == "failure" && response[:error][:code] == "RECORD_NOT_FOUND"
+
+      unless @user_riid.nil?
+        response = @client.retrieve_list_members(list, QueryColumn.new("RIID"), %w(EMAIL_PERMISSION_STATUS_), [@user_riid])
+
+        return false if response[:status] == "failure" && response[:error][:code] == "RECORD_NOT_FOUND"
+      end
+
+      true
+    end
+
+    def subscribed?(list)
+      return Responsys::Helper.format_response_with_message("member.record_not_found") unless present?(list)
+
+      response = @client.retrieve_list_members(list, QueryColumn.new("EMAIL_ADDRESS"), %w(EMAIL_PERMISSION_STATUS_), [@email])
+      response[:data][0][:EMAIL_PERMISSION_STATUS_] == "I"
     end
   end
 end

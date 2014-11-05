151d48e8959b4363d366b52ac0e6fca9c766093d
12and1studio <mkgriggs@gmail.com>
2014-11-05 10:44:04 -0800

    [UPDATE] Adding TriggerCustomEvent API call to the gem

--- lib/responsys/api/campaign.rb
+++ lib/responsys/api/campaign.rb
@@ -5,6 +5,15 @@ module Responsys
     module Campaign
       include Responsys::Exceptions
 
+      def trigger_custom_event(custom_event, recipients)
+        raise ParameterException, I18n.t("api.campaign.incorrect_recipients_type") unless recipients.is_a? Array
+        message = {
+          customEvent: custom_event.to_api,
+          recipientData: recipients.map(&:to_api)
+        }
+        api_method(:trigger_custom_event, message)
+      end
+
       def trigger_message(campaign, recipients)
         raise ParameterException, Responsys::Helper.get_message("api.campaign.incorrect_recipients_type") unless recipients.is_a? Array
         message = {

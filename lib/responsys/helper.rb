496c2e867428f057ac74ea35b1474aa6ff5ab1cb
Florian Lorrain <florian.lorrain@thredup.com>
2014-11-06 10:16:28 -0800

    [UPDATE] Rescue a specific exception in the get_message helper method

--- lib/responsys/helper.rb
+++ lib/responsys/helper.rb
@@ -83,7 +83,7 @@ module Responsys
     def self.get_message(key)
       begin
         I18n.t(key, scope: :responsys_api, locale: I18n.locale, raise: true)
-      rescue
+      rescue I18n::MissingTranslationData
         I18n.t(key, scope: :responsys_api, locale: :en)
       end
     end

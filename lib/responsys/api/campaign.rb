c88061d949161d0ed238218a03536adc8031b8e6
florrain <florian.lorrain@thredup.com>
2015-04-08 22:27:25 -0700

    [UPDATE] Unused method

--- lib/responsys/api/campaign.rb
+++ lib/responsys/api/campaign.rb
@@ -25,13 +25,6 @@ module Responsys
         api_method(:trigger_campaign_message, message)
       end
 
-      def check_failures(outcome, recipients)
-        if outcome.respond_to?(:each_index)
-          outcome.each_index { |i| puts "failed:\n" + recipients[i][:recipient].to_s unless outcome[i][:success] }
-        else
-          puts "failed:\n" + recipients[:recipient].to_s unless outcome[:success]
-        end
-      end
     end
   end
 end

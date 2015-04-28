bad47976b1d840dcf1aab92bf02c7d2678f3e1f9
James Thullbery <ifndef@gmail.com>
2015-04-28 13:37:10 +0200

    added a second parameter to the logic that checks the error that is returned from responsys

--- lib/responsys/member.rb
+++ lib/responsys/member.rb
@@ -69,7 +69,7 @@ module Responsys
         response = lookup_list_via_key(list, "EMAIL_ADDRESS", @email)
       end
 
-      !(response[:status] == "failure" && response[:error][:code] == "RECORD_NOT_FOUND")
+      !(response[:status] == "failure" && (response[:error][:code] == "RECORD_NOT_FOUND" || response[:error][:code] == "LIST_NOT_FOUND"))
     end
 
     def subscribed?(list)

9fd61d89e6de15fbda8ca82f840a35962bbb87b3
florrain <florian.lorrain@thredup.com>
2015-06-10 23:29:59 -0700

    [FIX] Failing specs

--- spec/api/object/recipient_spec.rb
+++ spec/api/object/recipient_spec.rb
@@ -1,68 +1,45 @@
 require "spec_helper.rb"
-require "responsys/api/client"
 
 describe Responsys::Api::Object::Recipient do
-
+  let(:recipient) { Responsys::Api::Object::Recipient.new }
 
   context "new recipient" do
 
-    before(:each) do
-      @recipient = Responsys::Api::Object::Recipient.new
-    end
-
     it "should have list_name attribute of type InteractObject" do
-      expect(@recipient.list_name).to be_a(Responsys::Api::Object::InteractObject)
+      expect(recipient.list_name).to be_nil
     end
 
     it "should have recipient_id attribute" do
-      expect(@recipient.recipient_id).to be_a(String)
+      expect(recipient.recipient_id).to be_nil
     end
 
     it "should have customer_id attribute" do
-      expect(@recipient.customer_id).to be_a(String)
+      expect(recipient.customer_id).to be_nil
     end
 
     it "should have email_address attribute" do
-      expect(@recipient.email_address).to be_a(String)
+      expect(recipient.email_address).to be_nil
     end
 
     it "should have mobile_number attribute" do
-      expect(@recipient.mobile_number).to be_a(String)
+      expect(recipient.mobile_number).to be_nil
     end
 
     it "should have email_format attribute of type EmailFormat" do
-      expect(@recipient.email_format).to be_a(Responsys::Api::Object::EmailFormat)
-    end
-  end
-
-  context "recipient to_api" do
-
-    before(:each) do
-      @recipient = Responsys::Api::Object::Recipient.new
+      expect(recipient.email_format).to be_nil
     end
 
-    it "should have listName attribute of type Hash" do
-      expect(@recipient.to_api[:listName]).to be_a(Hash)
-    end
-
-    it "should have recipientId attribute " do
-      expect(@recipient.to_api[:recipientId]).to be_a(Integer)
-    end
-
-    it "should have customerId attribute " do
-      expect(@recipient.to_api[:customerId]).to be_a(String)
-    end
-
-    it "should have emailAddress attribute " do
-      expect(@recipient.to_api[:emailAddress]).to be_a(String)
-    end
-
-    it "should have mobileNumber attribute " do
-      expect(@recipient.to_api[:mobileNumber]).to be_a(String)
-    end
-
-    it "should have EmailFormat attribute of type String" do
-      expect(@recipient.to_api[:emailFormat]).to be_a(String)
+    it "should correctly transform the object" do
+      expect(recipient.to_api).to eq(
+        {
+          listName: nil,
+          recipientId: nil,
+          customerId: nil,
+          emailAddress: nil,
+          mobileNumber: nil,
+          emailFormat: nil
+        }
+      )
     end
   end
 end
